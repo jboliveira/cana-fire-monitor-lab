8eb7c31ac75c650fb9bf3d09381d0ab9978379e2
Celio <juniorcelioata@gmail.com>
2025-06-01 18:53:25 -0300

    fix: Configuration baseUrl in application.properties

--- src/main/java/br/com/app/canafire/ingestion/HotspotDownloader.java
+++ src/main/java/br/com/app/canafire/ingestion/HotspotDownloader.java
@@ -9,6 +9,7 @@ import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
+import org.springframework.beans.factory.annotation.Value;
 
 @Component
 @RequiredArgsConstructor
@@ -17,15 +18,17 @@ public class HotspotDownloader {
     private final WebClient webClient;
     private final HotspotService hotspotService;
 
+    @Value("${app.base-url}")
+    private String baseUrl;
+
     private static final String CSV_URL =
             "https://dataserver-coids.inpe.br/queimadas/queimadas/focos/csv/10min/focos_10min_20250529_0000.csv";
 
     @Scheduled(fixedDelayString = "${app.pull-interval}")
     public void pullLatestCsv() {
         System.out.println("Start...");
-
         webClient.get()
-                .uri(CSV_URL)
+                .uri(baseUrl)
                 .retrieve()
                 .onStatus(
                         status -> status.is4xxClientError() || status.is5xxServerError(),
@@ -46,7 +49,4 @@ public class HotspotDownloader {
                 );
     }
 
-    public void RecpectorArchiveCSV(String CSV_URL) {
-        CSV_URL = "https://dataserver-coids.inpe.br/queimadas/queimadas/focos/csv/10min";
-    }
 }
