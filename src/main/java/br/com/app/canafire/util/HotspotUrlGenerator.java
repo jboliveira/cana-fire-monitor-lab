03fe2e584aa743f83d9bf8845e3f7536cb8d11c7
celio.vieira <juniorcelioata@gmail.com>
2025-06-09 12:36:24 -0300

    Criado HotspotUrlGenerator

--- src/main/java/br/com/app/canafire/util/HotspotUrlGenerator.java
+++ src/main/java/br/com/app/canafire/util/HotspotUrlGenerator.java
@@ -9,7 +9,7 @@ public class HotspotUrlGenerator {
 
     public static String generateLatestUrl(String baseUrl) {
         ZonedDateTime utcNow = ZonedDateTime.now(ZoneOffset.UTC).minusMinutes(10); // subtrai 10 minutos
-        int roundedMinute = (utcNow.getMinute() / 10) * 10; // calculo para arredondamento de 10 minutos
+        int roundedMinute = (utcNow.getMinute() / 10) * 10; // cálculo para arredondamento de 10 minutos
         utcNow = utcNow.withMinute(roundedMinute).withSecond(0).withNano(0); // minutos arredondados.segundos.milesegundos
 
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyyMMdd_HHmm");
